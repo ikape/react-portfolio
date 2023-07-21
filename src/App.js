3bb077e24f4ab0adf753ca4897c6d2b423d2aab4
Ikape Isaac <109391992+ikape@users.noreply.github.com>
2023-07-21 02:03:35 +0100

    minor

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { Skills } from './components/Skills';
 
 function App() {
 
-  const [darkMode, setDarkMode] = useState(false)
+  const [darkMode, setDarkMode] = useState(true)
 
   return (
     <div className={darkMode && 'dark'}>
